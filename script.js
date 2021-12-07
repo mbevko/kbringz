308b87180ca643f802ad26c6a3331c34c04a3311
Michael Evko <mbevko@gmail.com>
2021-12-07 11:39:48 -0500

    added mobile nav close on navigation

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ let mobileNav = document.querySelector(".mobile_nav");
 hamburgerMenu.addEventListener('click', () => {
     mobileNav.classList.toggle("mobile_nav_open")
 })
+
+// mobile nav close menu on navigation
+
+let listItems = document.querySelectorAll(".mobile_nav_list li");
+
+listItems.forEach(item => {
+    item.addEventListener('click', () => {
+        mobileNav.classList.toggle("mobile_nav_open");
+    })
+});
